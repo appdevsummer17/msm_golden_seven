bd0ee6a0a29bd725339b9595f38f5f506868f37f
Garrett Martin <me@garrettqmartin.com>
2017-07-20 16:34:48 +0000

    Add directors#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
+  
+  get '/directors' => 'directors#index'
 end
